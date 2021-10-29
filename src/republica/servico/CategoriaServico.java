8f0e71fe38d2d57e51ecda3533ba62621c1e9b94
pehenobra2 <peheguibra@gmail.com>
2021-10-29 15:05:56 -0300

    Melhoria na função cadastro de categoria

--- src/republica/servico/CategoriaServico.java
+++ src/republica/servico/CategoriaServico.java
@@ -1,13 +1,29 @@
 package republica.servico;
 
 import republica.modelo.Categoria;
-
 import java.util.List;
 import java.util.Optional;
+
+import javax.swing.JOptionPane;
 
 public class CategoriaServico extends Categoria {
 
     public boolean cadastrar(Categoria categoria) {
+    	
+    	categoria.descricao = JOptionPane.showInputDialog("Digite a descrição da categoria:");
+    	
+    	System.out.println(categoria.descricao);
+    	
+    	categoria.subcategoria = JOptionPane.showInputDialog("Digite a subcategoria:");
+    	
+    	System.out.println(categoria.subcategoria);
+    	
+    	String strId = JOptionPane.showInputDialog("Digite o id da categoria: ");
+    	
+    	categoria.id = Integer.parseInt(strId);
+    	
+    	System.out.println(categoria.id);
+    	
         return true;
     }
 
