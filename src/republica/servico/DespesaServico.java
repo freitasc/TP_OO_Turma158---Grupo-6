1f5f3d07a44cf989234089361e54beb7d6109846
Gabrielly Assunção <86726332+GabriellyAssuncao@users.noreply.github.com>
2021-10-30 15:27:26 -0300

    Update
    atualização

--- src/republica/servico/DespesaServico.java
+++ src/republica/servico/DespesaServico.java
@@ -1,25 +1,59 @@
 package republica.servico;
 
-import republica.modelo.Despesa;
-
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
+import javax.swing.JOptionPane;
+import republica.modelo.Despesa;
+import java.lang.String;
 
-public class DespesaServico {
+public class DespesaServico extends Despesa{
 
-    public boolean cadastrar(Despesa despesa) {
-        return true;
+    List<Despesa> despesas;
+    String nomeArquivo = "Despesas1.txt";
+
+    public DespesaServico() {
+        despesas = new LinkedList<Despesa>();
+        }
+    
+
+    public void cadastrar(republica.modelo.Despesa despesa){
+        descricao = JOptionPane.showInputDialog("Informe a descrição da             despesa: ");
+        String valorDp = JOptionPane.showInputDialog("Informe o valor da            despesa: ");
+        valor = Float.parseFloat(valorDp);
+        
+        /*Despesa d = new Despesa(descricao, valor); erro nesta linha*/
+
+        boolean resposta = despesas.add(d);
+        if (resposta)
+                  JOptionPane.showMessageDialog (null, "Despesa cadastrada com sucesso");
+        return;
+	 }
+    public void gravarAlunos() {
+		BufferedWriter buffer = null;
+		FileWriter out = null;
+		
+		try {
+			out = new FileWriter(nomeArquivo);
+			buffer = new BufferedWriter(out);
+			
+			for (Despesa d : despesas) {
+				buffer.write(d.toString());
+				buffer.write('\n');
+			}
+			
+			buffer.close();
+		} catch (IOException e) {
+			
+		}
     }
-
-    public boolean removerPorId(int id) {
-        return true;
-    }
-
-    public Optional<Despesa> buscarPorId(int id) {
-        return null;
-    }
-
-    public List<Despesa> listar() {
-        return null;
+			
+			buffer.close();
+		} catch (IOException e) {
+			
+		}
     }
 }
