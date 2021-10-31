f256316a8852f9786c5df5239668f9a9c5dca4c7
Gabrielly Assunção <86726332+GabriellyAssuncao@users.noreply.github.com>
2021-10-31 17:51:23 -0300

    Atualização 2
    listar despesa

--- src/republica/servico/DespesaServico.java
+++ src/republica/servico/DespesaServico.java
@@ -1,60 +1,87 @@
 package republica.servico;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import javax.swing.JOptionPane;
 import republica.modelo.Despesa;
 import java.lang.String;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-
-
-public class DespesaServico extends Despesa{
+public class DespesaServico extends Despesa {
 
     public boolean cadastrar(Despesa despesa) {
 
-		File bw = null;
-		FileWriter fw = null;
-		BufferedWriter buffWrite = null;
+        File bw = null;
+        FileWriter fw = null;
+        BufferedWriter buffWrite = null;
 
-		try {
+        try {
 
-			bw = new File("./Despesa.txt");
-			fw = new FileWriter(bw);
-			buffWrite = new BufferedWriter(fw);
-                        
-                        String idDp = JOptionPane.showInputDialog("Id da despesa: ");
-			despesa.id = Integer.parseInt(idDp);
-			System.out.println(despesa.id);
-			buffWrite.append(despesa.id + " \n ");
+            bw = new File("./Despesa.txt");
+            fw = new FileWriter(bw, true);
+            buffWrite = new BufferedWriter(fw);
 
-			despesa.descricao = JOptionPane.        showInputDialog("Digite a descrição da despesa: ");
-			System.out.println(despesa.descricao);
-			buffWrite.append(despesa.descricao + " | ");
+            String idDp = JOptionPane.showInputDialog("Id da despesa: ");
+            despesa.id = Integer.parseInt(idDp);
+            System.out.println(despesa.id);
+            buffWrite.append(despesa.id + ";");
 
-                        String valorDp = JOptionPane.showInputDialog("Digite o valor da despesa:");
-                        valor = Float.parseFloat(valorDp);
-			System.out.println(despesa.valor);
-			buffWrite.append(despesa.valor + " | ");
+            despesa.descricao = JOptionPane.showInputDialog("Digite a descrição da despesa: ");
+            System.out.println(despesa.descricao);
+            buffWrite.append(despesa.descricao + ";");
 
-			                 JOptionPane.showMessageDialog(null, "Despesa cadastrada com sucesso");
+            String valorDp = JOptionPane.showInputDialog("Digite o valor da despesa:");
+            valor = Float.parseFloat(valorDp);
+            System.out.println(despesa.valor);
+            buffWrite.append(despesa.valor + ".\n");
 
-			return true;
+            JOptionPane.showMessageDialog(null, "Despesa cadastrada com sucesso");
 
-		} catch (Exception ecxp) {
-			ecxp.printStackTrace(System.out);
-			return false;
-		} finally {
-			try {
-				buffWrite.close();
-			} catch (IOException ecxp) {
-				ecxp.printStackTrace();
-			}
-		}
-			
-		}
-   
+            return true;
+
+        } catch (Exception ecxp) {
+            ecxp.printStackTrace(System.out);
+            return false;
+        } finally {
+            try {
+                buffWrite.close();
+            } catch (IOException ecxp) {
+                ecxp.printStackTrace();
+            }
+        }
+
+    }
+
+    public void listar() {
+        List<String[]> lista = new ArrayList<>();
+        try {
+
+            FileReader fr = new FileReader("./Despesa.txt");
+            BufferedReader br = new BufferedReader(fr);
+
+            String str;
+            while ((str = br.readLine()) != null) {
+                lista.add(str.split(";"));
+            }
+            lista.forEach(a -> System.out.println(Arrays.toString(a)));
+
+            String textoArquivo = "";
+            for (String[] nome : lista) {
+                textoArquivo += nome + "\n";
+            }
+            JOptionPane.showMessageDialog(null, textoArquivo);
+
+        } catch (IOException e) {
+            System.out.println("Arquivo não encontrado!");
+
+        }
+    }
+
 }
-
 
