adec3061337623b65657372f4d9521f7bac892aa
Gabrielly Assunção <86726332+GabriellyAssuncao@users.noreply.github.com>
2021-11-01 15:59:17 -0300

    Atualização 3
    cadastrar e listar

--- src/republica/controlador/Republica.java
+++ src/republica/controlador/Republica.java
@@ -2,86 +2,86 @@ package republica.controlador;
 
 import java.io.IOException;
 import republica.servico.DespesaServico;
-import republica.servico.CategoriaServico;
 import javax.swing.JOptionPane;
 import republica.modelo.Despesa;
-import republica.modelo.Categoria;
-
 
 public class Republica {
-    public static void main(String[] args) throws IOException {
-    String strOpcao;
 
-	strOpcao = JOptionPane.showInputDialog("Bem vindo ao app de divisão de despesas mensais!!!"
-            + "\nEscolha uma opção:"
-            + "\n	1-Pessoas"
-            + "\n	2-Despesas"
-            + "\n	3-Categorias");
+    public static void main(String[] args) throws IOException {
+        String strOpcao;
+
+        strOpcao = JOptionPane.showInputDialog("Bem vindo ao app de divisão de despesas mensais!!!"
+                + "\nEscolha uma opção:"
+                + "\n	1-Pessoas"
+                + "\n	2-Despesas"
+                + "\n	3-Categorias");
 
         int opcao = Integer.parseInt(strOpcao);
 
-		System.out.println(opcao);
+        System.out.println(opcao);
 
-
-		switch(opcao) {
-
-		case 1: 
-
-			break;
-		case 2:
-        String opDep;
-        opDep = JOptionPane.showInputDialog("---DESPESAS---"
-		+ "\n	O que deseja fazer:"
-		+ "\n	1-Cadastrar despesa"
-		+ "\n	2-Listar despesa"
-		+ "\n	3-Buscar despesa por id"
-		+ "\n	4-Remover despesa");
-        int opCadDep = Integer.parseInt(opDep);
-
-	switch(opCadDep) {
+        switch (opcao) {
 
             case 1:
-		Despesa dp = new DespesaServico();    	
-		DespesaServico d = (DespesaServico) dp;
-		d.cadastrar(d);
-				
+
+                break;
             case 2:
-                Despesa a = new DespesaServico();    	
-		DespesaServico ad = (DespesaServico) a;
-		ad.listar();
-                
-				break;
-                
-			}
-			break;
-		
+                String opDep;
+                opDep = JOptionPane.showInputDialog("---DESPESAS---"
+                        + "\n	O que deseja fazer:"
+                        + "\n	1-Cadastrar despesa"
+                        + "\n	2-Listar despesa"
+                        + "\n	3-Buscar despesa por id"
+                        + "\n	4-Remover despesa");
+                int opCadDep = Integer.parseInt(opDep);
+
+                switch (opCadDep) {
+
+                    case 1:
+                        Despesa d = new Despesa();
+
+                        String strID = JOptionPane.showInputDialog(null, "Id da Despesa: ");
+                        int id = Integer.parseInt(strID);
+                        d.setId(id);
+
+                        String Descricao = JOptionPane.showInputDialog(null, "Digite a descrição da despesa: ");
+                        d.setDescricao(Descricao);
+
+                        String valorDp = JOptionPane.showInputDialog(null, "Digite o valor da despesa: ");
+                        float valor = Float.parseFloat(valorDp);
+                        d.setValor(valor);
+
+                        DespesaServico despesa = new DespesaServico();
+                        despesa.cadastrar(d);
+                        break;
+
+                    case 2:
+                        DespesaServico lista = new DespesaServico();
+                        String listarDp = "";
+                        for (Despesa dp : lista.listar()) {
+                            listarDp = (listarDp + dp.getId() + " | "
+                                    + dp.getDescricao() + " | "
+                                    + dp.getValor() + " |\n");
+
+                        }
+                        JOptionPane.showMessageDialog(null, listarDp);
+                        break;
+                    case 3:
+                          break;
+                    case 4:
+                        break;
+                }
+                break;
+
             case 3:
-	String strOpcaoCat;
-
-	strOpcaoCat = JOptionPane.showInputDialog("|CATEGORIAS|"
-		+ "\n	O que deseja fazer:"
-		+ "\n	1-Cadastrar categoria"
-		+ "\n	2-Listar categorias"
-		+ "\n	3-Buscar categoria por id"
-		+ "\n	4-Remover categoria");
-
-	int opcaoCat = Integer.parseInt(strOpcaoCat);
-
-	switch(opcaoCat) {
-
-            case 1:
-		Categoria cat = new CategoriaServico();    	
-		CategoriaServico a = (CategoriaServico) cat;
-		a.cadastrar(a);
-				break;	
-			}
-			break;
-		}
-
-
+                
+                        break;
+                }
+               
+        }
 
     }
-    
-}
+
+
 
 
